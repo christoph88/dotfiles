ed1da7130a3bfd15c6c3a4c669ddea9af9f95b52
Christoph Geypen <geypenchristoph@gmail.com>
2019-04-04 15:40:57 +0200

    add vim directory creation

--- setup.sh
+++ setup.sh
@@ -1,7 +1,12 @@
 #!/bin/bash 
 
+# create symbolic links
 ln -sv ~/.dotfiles/.bashrc ~
 ln -sv ~/.dotfiles/.inputrc ~
 ln -sv ~/.dotfiles/.vimrc ~
 ln -sv ~/.dotfiles/.tmux.conf ~
+
+# create vim swap and backup directory
+mkdir ~/.vimswap
+mkdir ~/.vimbackup
 
