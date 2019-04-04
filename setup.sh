291a9cef0750cc201debc0d6394cacf006ab9530
Christoph Geypen <geypenchristoph@gmail.com>
2019-04-04 15:34:59 +0200

    correct dot error in setup

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash 
 
-ln -sv ~/Projects/dotfiles/.bash_profile ~
-ln -sv ~/Projects/dotfiles/.inputrc ~
-ln -sv ~/Projects/dotfiles/.vimrc ~
-ln -sv ~/Projects/dotfiles/.tmux.conf ~
+ln -sv ~/Projects/.dotfiles/.bashrc ~
+ln -sv ~/Projects/.dotfiles/.inputrc ~
+ln -sv ~/Projects/.dotfiles/.vimrc ~
+ln -sv ~/Projects/.dotfiles/.tmux.conf ~
 
